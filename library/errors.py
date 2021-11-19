2bdbd6360597da10a905b601d95b122aeef4a873
Willbeckh <wileyhndirangu04@gmail.com>
2021-11-20 02:36:25 +0300

    update errors

--- library/errors.py
+++ library/errors.py
@@ -8,4 +8,4 @@ def not_found_err(error):
 
 @app.errorhandler(500)
 def internal_error(error):
-    return jsonify({"message": "oops! Internal server error"}), 500
+    return jsonify({"message": "Internal Server error!"}), 500
